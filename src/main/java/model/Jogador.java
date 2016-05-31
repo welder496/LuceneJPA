8b8edcd1b290cba7239de29129f92e95d8f37b32
welder496 <welder496@gmail.com>
2016-05-31 16:36:22 -0300

    Atualização do pom.xml

--- src/main/java/model/Jogador.java
+++ src/main/java/model/Jogador.java
@@ -1,5 +1,30 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Jogador {
 
+	@Id
+	@GeneratedValue
+	private int codigo;
+	
+	private String nome;
+
+	private float atual;
+	
+	private float ultima;
+	
+	private float media;
+		
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
