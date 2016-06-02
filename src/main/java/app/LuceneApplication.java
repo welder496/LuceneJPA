d2ec673da48194e0dbb826bfce47ec869761d465
welder496 <welder496@gmail.com>
2016-06-02 10:53:43 -0300

    Mudança do log4j.properties

--- src/main/java/app/LuceneApplication.java
+++ src/main/java/app/LuceneApplication.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.apache.log4j.Logger;
 
 public class LuceneApplication {
 
