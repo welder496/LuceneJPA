fa712ae99a4836ee88a25c1dfa54b53fe9eae342
welder496 <welder496@gmail.com>
2016-06-02 10:23:38 -0300

    Criação do arquivo de propriedades do log4j

--- src/main/java/model/Jogador.java
+++ src/main/java/model/Jogador.java
@@ -1,11 +1,13 @@
 package model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Jogador {
+public class Jogador implements Serializable {
 
 	@Id
 	@GeneratedValue
