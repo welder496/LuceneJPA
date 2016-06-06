8210e9943fea7c67fe344fb2f1acdf9aa365bbbe
welder496 <welder496@gmail.com>
2016-06-06 11:43:21 -0300

    Adição do recurso: context-application.xml

--- src/main/java/controller/JogadorControler.java
+++ src/main/java/controller/JogadorControler.java
@@ -1,5 +1,12 @@
 package controller;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "singleton")
 public class JogadorControler {
 
+	
+	
 }
